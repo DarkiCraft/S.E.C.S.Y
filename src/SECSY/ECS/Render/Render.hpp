663408d2da565bf2e7ed59b17ff764b521836f24
redsteadz <harcolemas@gmail.com>
2025-08-05 17:59:38 +0500

    feat: add scaling mode and rendering updates for dynamic window resizing

--- src/SECSY/ECS/Render/Render.hpp
+++ src/SECSY/ECS/Render/Render.hpp
@@ -2,6 +2,7 @@
 
 #include <cstdint>
 #include <string>
+#include "cmath"
 #include "raylib.h"
 
 namespace SECSY {
@@ -30,9 +31,52 @@ class Render {
     return IsWindowFullscreen();
   }
 
+  void SetScalingMode(ScalingMode mode) {
+    scalingMode = mode;
+    UpdateScaling();
+  }
+
+  void BeginRendering() {
+    if (!WindowShouldClose() && (GetScreenWidth() != currentWindowWidth ||
+                                 GetScreenHeight() != currentWindowHeight)) {
+      UpdateScaling();
+    }
+  }
+
  private:
   std::int32_t width;
+  std::int32_t currentWindowWidth;
+  std::int32_t currentWindowHeight;
   std::int32_t height;
   std::string title;
+
+  ScalingMode scalingMode;
+  float renderScale;
+  Vector2 renderOffset;
+  RenderTexture2D renderTexture;
+
+  void UpdateScaling() {
+    currentWindowWidth  = GetScreenWidth();
+    currentWindowHeight = GetScreenHeight();
+
+    float scaleX = (float)currentWindowWidth / width;
+    float scaleY = (float)currentWindowHeight / height;
+
+    switch (scalingMode) {
+      case ScalingMode::LETTERBOX:
+        renderScale = std::min(scaleX, scaleY);
+        break;
+
+      case ScalingMode::PIXEL_PERFECT:
+        renderScale = std::max(1.0f, std::floor(std::min(scaleX, scaleY)));
+        break;
+    }
+
+    float scaledWidth  = width * renderScale;
+    float scaledHeight = height * renderScale;
+
+    renderOffset.x = (currentWindowWidth - scaledWidth) / 2.0f;
+    renderOffset.y = (currentWindowHeight - scaledHeight) / 2.0f;
+  }
 };
 }  // namespace SECSY
